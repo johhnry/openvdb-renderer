dd1c7216fe082f9e7f843401f4a2d752f207f7c4
johhnry <josephenry@protonmail.com>
2021-05-11 20:10:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenVDB OpenGL renderer
+
+Foobar is a Python library for dealing with word pluralization.
+
+## Sources
+- https://github.com/morizotter/imgui-opengl-glfw-glew-cmake-demo
+- https://retifrav.github.io/blog/2019/05/26/sdl-imgui/
---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include <openvdb/Types.h>
+#include <openvdb/openvdb.h>
+#include <openvdb/tools/LevelSetSphere.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw_gl3.h>
+#include <cstdlib>
+#include <cstdio>
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+int main(int, char**)
+{
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        return 1;
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    glewInit();
+
+    // Setup ImGui binding
+    ImGui_ImplGlfwGL3_Init(window, true);
+
+    bool show_test_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImColor(114, 144, 154);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            if (ImGui::Button("Test Window")) show_test_window ^= 1;
+            if (ImGui::Button("Another Window")) show_another_window ^= 1;
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        if (show_another_window)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
+
+        if (show_test_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+            ImGui::ShowTestWindow(&show_test_window);
+        }
+
+        // Rendering
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    ImGui_ImplGlfwGL3_Shutdown();
+    glfwTerminate();
+
+    return 0;
+}
